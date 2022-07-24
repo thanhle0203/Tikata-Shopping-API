8c2ef1b6b749d781497bd3d4045ae8c7b7aa3149
Thanh Le <thanhlevan0203@gmail.com>
2022-07-24 00:31:05 -0700

    Created API & routers users

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Load Express module
 const express = require("express");
 const app = express();
-
 //Connect to mongose db
 const mongoose = require("mongoose");
-
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
 dotenv.config();
+
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -14,6 +15,10 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  // Created API user
+app.use(express.json());
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Backend server is running!");
