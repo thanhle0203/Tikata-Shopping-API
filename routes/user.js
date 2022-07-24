3e79dce7856eee739c107c877b8f57a5a5125539
Thanh Le <thanhlevan0203@gmail.com>
2022-07-24 16:10:17 -0700

    Created CRUD Operation with JWT

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,26 @@
 const router = require("express").Router();
+const { verify } = require("jsonwebtoken");
 const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+ 
+// UPDATE USER
+router.put("/:id", verifyTokenAndAuthorization, async (req,res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id, {
+        $set: req.body,
+      }, 
+      {new: true})
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
