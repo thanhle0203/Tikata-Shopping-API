8859c3b1e23a0396ecd1be548aa18ceeb93cfa4d
Thanh Le <thanhlevan0203@gmail.com>
2022-07-24 14:42:25 -0700

    New Updated Product, User models, $ user routes

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,14 @@
 const mongoose = reqire("mongoose");
+
 const ProductSchema = new mongoose.Schema(
   {
-    title: { type:String, required: true, unique: true},
-    desc: {type:String, required: true},
-    img: {type:String, required: true},
+    title: { type: String, required: true, unique: true},
+    desc: {type: String, required: true},
+    img: {type: String, required: true},
     categories: {type: Array, required:true},
-    size: {type:String},
-    color: {type:String, required:true}, 
-    price: {type:String, required:true},
+    size: {type: String},
+    color: {type: String, required:true}, 
+    price: {type: String, required:true},
   },
   { timestamps: true }
 )
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,4 @@
 const router = require("express").Router();
-
-router.get("/usertest", (req,res) => {
-  res.send("user test is successful");
-});
-
-router.post("/userposttest", (req,res) => {
-  const username = req.body.username;
-  res.send("your username is: " + username);
-});
+const User = require("../models/User");
 
 module.exports = router;
