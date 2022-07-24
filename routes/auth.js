b8cd333f9055a8d7d9cc3fa550e4467f061e44ad
Thanh Le <thanhlevan0203@gmail.com>
2022-07-24 15:33:28 -0700

    Hidden Password

--- routes/auth.js
+++ routes/auth.js
@@ -27,10 +27,13 @@ router.post("/login", async (req,res) => {
     !user && res.status(401).json("Wrong credentials!");
 
     const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
-    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-    password !== req.body.password && res.status(401).json("Wrong credentials!");
-    res.status(200).json(user);
+    originalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
+
+    const { password, ...others} = user._doc;
+
+    res.status(200).json(others);
 
   } catch (err) {
     res.status(500).json(err);
